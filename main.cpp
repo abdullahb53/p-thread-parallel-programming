9e047ff1ebefd381108167f509be08b5d017bc35
abdullahb53 <abdullahb5355@gmail.com>
2022-04-23 15:47:14 +0300

    globalpi-buffer-add

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
     #include <sys/time.h>
     #include <mutex>
 
-    #define N 20000000
-    #define NTHREADS 8
+    #define N 202300072
+    #define NTHREADS 5
 
 using namespace std;
 
@@ -16,58 +16,45 @@ using namespace std;
 pthread_mutex_t kilit = PTHREAD_MUTEX_INITIALIZER;
 
 double globalPi = 0.0;
+int mod = (N%NTHREADS);
+int buffer = (N / NTHREADS) + mod;
 
 
 double ownPi[NTHREADS] = {0.0};
 
 
 void  *helloLoop1(void* pArg){
-
-    double x;
-    double sum;
-
-    double step = 1.0 / N;
-
+    //cout << "\n N/NTHREADS ="<<N/NTHREADS<<"\n N%NTHREADS="<<N%NTHREADS<<"\n N="<<N<<"\n buffer ="<<buffer<< "\n buffer * nthreads="<<buffer * NTHREADS<<"\n NThreads = "<<NTHREADS<<"\n";
     int myId = *((int*)pArg);
-    cout << "\n my Thread id = " << myId;
-    for(int i = myId;i<N;i+=NTHREADS){
-        x = (i + 0.5f)*step;
-        pthread_mutex_lock(&kilit);
-        globalPi += step*4.0/(1.0 + x*x);
-        pthread_mutex_unlock(&kilit);
+    double x,pi,step;
+    pi = 0;
+    step = 1.0 / (double) (buffer * NTHREADS);
+
+     pthread_mutex_lock(&kilit);
+     cout << "\nmy Thread id: " << myId<<"\n";
+     pthread_mutex_unlock(&kilit);
+    for(int i = myId*buffer;i < (buffer*(myId+1));i++){
+        x = (i + 0.5f) * step;
+        pi = pi + ( step *  4.0/(1.0+x*x) );
     }
 
-
-    //ownPi[myId] = sum;
-
+    pthread_mutex_lock(&kilit);
+    globalPi += pi;
+    pthread_mutex_unlock(&kilit);
 }
 
 void pThreadFunc(){
-
     pthread_t threadTutucu[NTHREADS];
     int threadNumbers[NTHREADS];
 
     for(int j=0;j<NTHREADS;++j){
         threadNumbers[j] = j;
         pthread_create(&threadTutucu[j],NULL,helloLoop1,(void*)&threadNumbers[j]);
-
     }
 
     for(int i = 0;i<NTHREADS; ++i){
         pthread_join (threadTutucu[i],NULL);
-
-
     }
-
-    /*for(int i = 0;i<NTHREADS; ++i){
-
-    globalPi += ownPi[i];
-
-    }*/
-
-
-
-
 }
 
 
@@ -76,15 +63,13 @@ int main()
     timeval tim;
     gettimeofday(&tim,NULL);
     double t1 = 1.0e6 * tim.tv_sec + tim.tv_usec;
-    //---FUNC---
+    //---FUNCTION---
     pThreadFunc();
-    //---FUNC---
+    //---FUNCTION---
     gettimeofday(&tim,NULL);
     double t2 = 1.0e6 * tim.tv_sec + tim.tv_usec;
-
     cout <<"\n[Function Void Time] = "<<t2-t1;
     cout <<"\n[Global Pi Degiskeni=]"<<globalPi;
-
-    //cout << "Hello world!" << endl;
+    getchar();
     return 0;
 }
